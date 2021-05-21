16131b62abc03729eae41aac74e3fa44c5a714db
JesseLuo <luo_sj@163.com>
2021-05-21 16:15:00 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <button @click="showImages">测试图片组件</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ImageViewer from './components/image-viewer'
+
+export default {
+  components: {
+    HelloWorld
+  },
+  setup () {
+    const showImages = () =>{
+      ImageViewer()
+    }
+
+    return {
+      showImages
+    }
+  }
+}
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
